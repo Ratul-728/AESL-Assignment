307a4fb4d8f5aea808a270120106ac4b7779e29f
Ratul-728 <s.m.alam@northsouth.edu>
2022-10-06 16:06:02 +0600

    Security 1st phase implemented

--- src/main/java/Assignment/Assignment/userException/UserExceptionHandler.java
+++ src/main/java/Assignment/Assignment/userException/UserExceptionHandler.java
@@ -8,10 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.sql.SQLIntegrityConstraintViolationException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @ControllerAdvice
 public class UserExceptionHandler {
@@ -33,11 +30,12 @@ public class UserExceptionHandler {
         List<String> errMsg = new ArrayList<>();
         Map<String, String> map = new HashMap<>();
 
-        for (int i = 0; i < ex.getAllErrors().size(); i++) {
-            map.put(ex.getFieldErrors().get(i).getField()
-                    ,ex.getAllErrors().get(i).getDefaultMessage());
+        for (int i = 0; i < ex.getErrorCount(); i++) {
+            errMsg.add(ex.getFieldErrors().get(i).getField()+": "+ex.getAllErrors().get(i).getDefaultMessage());
+//            map.put(ex.getFieldErrors().get(i).getField()
+//                    ,ex.getAllErrors().get(i).getDefaultMessage());
         }
-        userResponse.setErrorData(map);
+        userResponse.setErrorData(errMsg);
         userResponse.setStatus("BAD_REQUEST");
         userResponse.setData(null);
         return ResponseEntity.badRequest().body(userResponse);
